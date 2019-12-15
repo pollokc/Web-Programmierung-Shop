c72a0d66cba5ac2144804c914da5d5a1c8a58515
MarkPhilipps <m.h.philipps@gmail.com>
2019-12-15 17:09:22 +0100

    Login/Register Fixxed

--- sql/InsertDemo.sql
+++ sql/InsertDemo.sql
@@ -6,7 +6,7 @@ INSERT INTO `produkt` (`id`,`produktname`, `preis`, `beschreibung`) VALUES
 (5, 'Erdbeer-Schoko im Doppelpack', 7.99, 'Kreative Beschreibung');
 
 INSERT INTO `benutzer` (`id`,`email`,`passwort`,`salz`,`vorname`,`nachname`) VALUES
-(1,'test@test.de','987b0584c39d0b40505d02ad2c30ce96d65762004f5fa71ed36d1641b5aac31e59e396384b0ba7d2c22b9abc8daa032247253abe0fccc925dd0eaa028400874c','19594914065df61406b6ea05.32069662', 'TestVorname', 'TestNachname');
+(1,'test@test.de','1b3df60c735116b37986b67b1a006924785d009c9476f4206e773b1df7eb308cb94700e4bba2d0c7b9443bde6b95a902055cb2378b276c15d4a0bdb39d61432e','19594914065df61406b6ea05.32069662', 'TestVorname', 'TestNachname');
 
 INSERT INTO `adresse` (`id`, `benutzerid`, `vornachname`, `zusatzinfo`, `strasse`, `hausnummer`, `plz`, `ort`) VALUES
 ('1', '1', 'TestAdresse1', 'TestFirma', 'Teststrasse', '4', '72764', 'Reutlingen'),
