2827637d2d7575a200ea692c5016c27841721034
MarkPhilipps <m.h.philipps@gmail.com>
2019-12-23 14:38:34 +0100

    Doppelte Abhängigkeiten gelöscht

--- main.php
+++ main.php
@@ -33,7 +33,6 @@ $statement = $pdo->prepare("SELECT * FROM produkt");
     <link rel="stylesheet" href="css/default.css">
 
 
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
     <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
     <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
@@ -185,10 +184,7 @@ $statement = $pdo->prepare("SELECT * FROM produkt");
 
 
 
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
     <script src="vendor/animsition/js/animsition.min.js"></script>
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="vendor/select2/select2.min.js"></script>
     <script src="vendor/daterangepicker/moment.min.js"></script>
     <script src="vendor/daterangepicker/daterangepicker.js"></script>
