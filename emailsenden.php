8e23357e179ae2660b9d786b88bc99061a818366
MarkPhilipps <m.h.philipps@gmail.com>
2020-01-03 14:32:44 +0100

    Email bei Bestellung versenden

--- emailsenden.php
+++ emailsenden.php
@@ -7,7 +7,6 @@
         $email->setFrom("thejuiceboxwi3@gmail.com", "TheJuiceBox-WebShop");
         $email->setSubject($betreff);
         $email->addTo($empfaenger);
-        //$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
         $email->addContent(
             "text/html", $nachricht
         );
@@ -24,7 +23,33 @@
         $userStatement = $pdo->prepare("SELECT * FROM `benutzer` WHERE id = :id;");
         $userStatement->execute(array('id' => $userid));
         $user = $userStatement->fetch();
-        $message = "UserID: ".$userid."<br>BestellID: ".$bestellungid;
+        $bestellungStatement = $pdo->prepare("SELECT * FROM `bestellung` WHERE id = :id;");
+        $bestellungStatement->execute(array('id' => $bestellungid));
+        $bestellung = $bestellungStatement->fetch();
+        $bestellungProdukteStatement = $pdo->prepare("SELECT * FROM `bestellung_hat_produkte` WHERE bestellungid = :id;");
+        $bestellungProdukteStatement->execute(array('id' => $bestellungid));
+        $bestellungProdukte = $bestellungProdukteStatement->fetchAll();
+        $message = "Vielen Dank für Ihre Bestellung bei TheJuiceBox.<br>    
+        <br>Bestellnr.: ".$bestellung["id"].
+        "<br>Artikelkosten: ".$bestellung["summe"]." €";
+        if($bestellung["expresslieferung"] == 1)
+        {
+            $message .= "<br>Lieferkosten: 5.00 €"
+            ."<br>Gesamtkosten: ".($bestellung["summe"]+5)." €";
+        }
+        else
+        {
+            $message .= "<br>Lieferkosten: 0.00 €"
+            ."<br>Gesamtkosten: ".$bestellung["summe"]." €";
+        }
+        
+
+        $message .= "<br><br>Gekaufte Produkte:<br>";
+        foreach($bestellungProdukte as $orderProduct)
+        {
+            $message .= $orderProduct["menge"]."x ".$products[$orderProduct["produktid"]-1]["produktname"]."<br>";
+        }
+        $message .= "<br><br>Lieferdatum: nie<br><br><br>Mit freundlichen Grüßen<br>Ihr TheJuiceBox-WebShop";
         senden($user["email"], "Versandbestätigung", $message);
     }
 ?>
