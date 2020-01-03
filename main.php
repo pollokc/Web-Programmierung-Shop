83373e0b09d53aa2b66424122160da9bdff8e267
MarkPhilipps <m.h.philipps@gmail.com>
2020-01-03 13:07:25 +0100

    File Format

--- main.php
+++ main.php
@@ -1,151 +1,130 @@
 <?php
-function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Output: ".$output. "' );</script>";
-}
-
-session_start();
-if(empty($_SESSION["userid"]))
-{
+  session_start();
+  if(empty($_SESSION["userid"]))
+  {
     header("Location: index.php");
     die();
-}
-
-$pdo = new PDO('mysql:host=localhost;dbname=thejuicebox', 'root', '');
-$statement = $pdo->prepare("SELECT * FROM produkt");
-    //$result = $statement->execute();
-    $statement->execute();
-    $products = $statement->fetchAll();
-
-    $statement = $pdo->prepare("SELECT * FROM benutzer WHERE id = :id");
-    $statement->execute(array('id' => $_SESSION['userid']));
-    $user = $statement->fetch();
-
-
-if (isset($_GET['action']) and $_GET['action']=='addCart') {
-  $userid = $_SESSION["userid"];
-  $productid = $_GET['id'];
-  $getWarenkorb = $pdo->prepare("SELECT * FROM warenkorb WHERE benutzerid = :user AND produktid = :product");
-  $getWarenkorb->execute(array('user' => $userid, 'product' => $productid));
-  $warenkorb = $getWarenkorb->fetch();
-  if(!empty($warenkorb)){
-    $menge = $warenkorb['menge'];
-    $menge++;
-    $insertProduct = $pdo->prepare("UPDATE `warenkorb` SET `menge`= :menge WHERE benutzerid = :user AND produktid = :product");
-    $insertProduct->execute(array('menge' => $menge, 'user' => $userid, 'product' => $productid));
   }
-  else{
-    $insertProduct = $pdo->prepare("INSERT INTO `warenkorb`(`benutzerid`, `produktid`, `menge`) VALUES (:user,:product,1)");
-    $insertProduct->execute(array('user' => $userid, 'product' => $productid));
-  }
-  if(isset($_GET['fast'])){
-    header("Location: warenkorb.php");
-    die();
-  }
+  $pdo = new PDO('mysql:host=localhost;dbname=thejuicebox', 'root', '');
+  $statement = $pdo->prepare("SELECT * FROM produkt");
+  $statement->execute();
+  $products = $statement->fetchAll();
+  $statement = $pdo->prepare("SELECT * FROM benutzer WHERE id = :id");
+  $statement->execute(array('id' => $_SESSION['userid']));
+  $user = $statement->fetch();
+  if (isset($_GET['action']) and $_GET['action']=='addCart') {
+    $userid = $_SESSION["userid"];
+    $productid = $_GET['id'];
+    $getWarenkorb = $pdo->prepare("SELECT * FROM warenkorb WHERE benutzerid = :user AND produktid = :product");
+    $getWarenkorb->execute(array('user' => $userid, 'product' => $productid));
+    $warenkorb = $getWarenkorb->fetch();
+    if(!empty($warenkorb)){
+      $menge = $warenkorb['menge'];
+      $menge++;
+      $insertProduct = $pdo->prepare("UPDATE `warenkorb` SET `menge`= :menge WHERE benutzerid = :user AND produktid = :product");
+      $insertProduct->execute(array('menge' => $menge, 'user' => $userid, 'product' => $productid));
+    }
+    else{
+      $insertProduct = $pdo->prepare("INSERT INTO `warenkorb`(`benutzerid`, `produktid`, `menge`) VALUES (:user,:product,1)");
+      $insertProduct->execute(array('user' => $userid, 'product' => $productid));
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="de">
-<head>
-  <!-- Bootstrap Abhängikeiten -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <script src="bootstrap/js/jquery.min.js"></script>
-  <script src="bootstrap/js/popper.min.js"></script>
-  <script src="bootstrap/js/bootstrap.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Default Settings -->
-  <title>The Juice Box</title>
-  <meta charset="UTF-8">
-  <link rel="icon" type="image/png" href="images/icon/smoothie64.png">
-  <link rel="stylesheet" href="css/default.css">
-  <!-- Sonstige Settings -->
-  <script src="js/logged_in.js"></script>
-  <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-    <a class="navbar-brand" href="main.php"><img src="images/icon/smoothie512.png" width="35" height="35" alt=""></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="warenkorb.php">Warenkorb</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="bestellungen.php">Meine Bestellungen</a>
-      </li>
-    </ul>
-    
-    <a class="btn btn-outline-primary" href="logout.php" role="button">Logout</a>     
-  </span>
-  </nav>
-  <div class="welcome-Text">
-    Herzlich Willlkommen <?php echo $user['vorname'].' '.$user['nachname'] ?>! Zuletzt online <?php echo date('d.m.Y', strtotime($user['last_login'])) ?> 
-  </div>
-</header> <!-- Ende der Navigation und Willkomens-Label  -->
-<main>
-<div id="carousel" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="images/carousel/smoothie1.jpg">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images/carousel/smoothie2.jpg">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images/carousel/smoothie3.jpg">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images/carousel/smoothie4.jpg">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images/carousel/smoothie5.jpg">
-    </div>
-  </div>
-</div> <!-- Ende des Karusells -->
-<center>
-    <a class="btn btn-primary m-5" id="scrolldownAction" href="#produktuebersicht">
-    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-        Zu den Produkten
-    </a>
-    <script src="js/scrolldown.js"></script>
-    </center>
-    <center>
-      <div class="container-fluid" id="produktuebersicht">
-      <div class= "row d-inline-flex">
-        <?php foreach ($products as $product): ?>
-        <div class="col-sm mb-5">
-          <div class="card">
-            <div class="card-image">
-              <img src="images/products/product<?php echo $product["id"]?>.png" class="card-img-top">
-            </div>
-              <div class="card-body">
-              <h5 class="card-title"><?php echo $product["hersteller"].' '.$product["produktname"]?></h5>
-              <p class="card-text"><?php echo $product["beschreibung"]?></p>
-              <p class="card-text price-text"><?php echo $product["preis"] ?>€</p>
-              <div class="card-buttons">
-                <a class="btn btn-outline-primary mt-3" href="?action=addCart&id=<?php echo $product["id"] ?>&fast=true">Jetzt bestellen!</a>
-                <a class="btn btn-outline-secondary mt-3" href="?action=addCart&id=<?php echo $product["id"] ?>">Warenkorb hinzufügen!</a>
-              </div>
-            </div>
-            </div>
-          </div>
-      <?php endforeach; ?>
+  <head>
+    <!-- Bootstrap Abhängikeiten -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Default Settings -->
+    <title>The Juice Box</title>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="images/icon/smoothie64.png">
+    <link rel="stylesheet" href="css/default.css">
+    <!-- Sonstige Settings -->
+    <script src="js/logged_in.js"></script>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <a class="navbar-brand" href="main.php"><img src="images/icon/smoothie512.png" width="35" height="35" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="warenkorb.php">Warenkorb</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="bestellungen.php">Meine Bestellungen</a>
+          </li>
+          </ul>
+          <a class="btn btn-outline-primary" href="logout.php" role="button">Logout</a>
+        </div>     
+      </nav>
+      <div class="welcome-Text">
+        Herzlich Willlkommen <?php echo $user['vorname'].' '.$user['nachname'] ?>! Zuletzt online <?php echo date('d.m.Y', strtotime($user['last_login'])) ?> 
       </div>
-    </div></center>
-    
-</main>
-<footer>
-  <div class="logged-in-text">
-    <span id="logged_in"></span>&nbsp;<br>
-  </div>
-</footer>
-</body>
+    </header> <!-- Ende der Navigation und Willkomens-Label  -->
+    <main>
+      <div id="carousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="images/carousel/smoothie1.jpg">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/carousel/smoothie2.jpg">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/carousel/smoothie3.jpg">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/carousel/smoothie4.jpg">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/carousel/smoothie5.jpg">
+          </div>
+        </div>
+      </div>
+      <center>
+        <a class="btn btn-primary m-5" id="scrolldownAction" href="#produktuebersicht">
+          <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+          Zu den Produkten
+        </a>
+        <script src="js/scrolldown.js"></script>
+        <div class="container-fluid" id="produktuebersicht">
+          <div class= "row d-inline-flex">
+            <?php foreach ($products as $product): ?>
+              <div class="col-sm mb-5">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="images/products/product<?php echo $product["id"]?>.png" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title"><?php echo $product["hersteller"].' '.$product["produktname"]?></h5>
+                    <p class="card-text"><?php echo $product["beschreibung"]?></p>
+                    <p class="card-text price-text"><?php echo $product["preis"] ?>€</p>
+                    <div class="card-buttons">
+                      <a class="btn btn-outline-primary mt-3" href="?action=addCart&id=<?php echo $product["id"] ?>&fast=true">Jetzt bestellen!</a>
+                      <a class="btn btn-outline-secondary mt-3" href="?action=addCart&id=<?php echo $product["id"] ?>">Warenkorb hinzufügen!</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      </center>
+    </main>
+    <footer>
+      <div class="logged-in-text">
+        <span id="logged_in"></span>&nbsp;<br>
+      </div>
+    </footer>
+  </body>
 </html>
--- warenkorb.php
+++ warenkorb.php
@@ -1,37 +1,38 @@
 <?php
-session_start();
-if(empty($_SESSION["userid"]))
-{
+  session_start();
+  if(empty($_SESSION["userid"]))
+  {
     header("Location: index.php");
-}
-
-$pdo = new PDO('mysql:host=localhost;dbname=thejuicebox', 'root', '');
-    $warenkorb = $pdo->prepare("SELECT * FROM `warenkorb` WHERE benutzerid = :id;");
-    $warenkorb->execute(array('id' => $_SESSION['userid']));
-    $userWarenkorb = $warenkorb->fetchAll();
-
-    $produktStatement = $pdo->prepare("SELECT * FROM `produkt`");
-    $produktStatement->execute(array('id' => $_SESSION['userid']));
-    $products = $produktStatement->fetchAll();
-
-
-  if(isset($_GET['action'])){
+  }
+  $pdo = new PDO('mysql:host=localhost;dbname=thejuicebox', 'root', '');
+  $warenkorb = $pdo->prepare("SELECT * FROM `warenkorb` WHERE benutzerid = :id;");
+  $warenkorb->execute(array('id' => $_SESSION['userid']));
+  $userWarenkorb = $warenkorb->fetchAll();
+  $produktStatement = $pdo->prepare("SELECT * FROM `produkt`");
+  $produktStatement->execute(array('id' => $_SESSION['userid']));
+  $products = $produktStatement->fetchAll();
+  if(isset($_GET['action']))
+  {
     $userid = $_SESSION["userid"];
-    if($_GET['action']=='delete'){
+    if($_GET['action']=='delete')
+    {
       $productid = $_GET['id'];
       $deleteItem = $pdo->prepare("DELETE FROM `warenkorb` WHERE benutzerid = :user AND id = :product");
       $deleteItem->execute(array('user' => $userid, 'product' => $productid));
       header("Location: warenkorb.php");
     }
-    if($_GET['action']=='increase' or $_GET['action']=='decrease'){
+    if($_GET['action']=='increase' or $_GET['action']=='decrease')
+    {
       $productid = $_GET['id'];
       $selectItem = $pdo->prepare("SELECT * FROM `warenkorb` WHERE `benutzerid` = :user AND `produktid` = :product;");
       $selectItem->execute(array('user' => $userid, 'product' => $productid));
       $item = $selectItem->fetch();
-       if($_GET['action']=='decrease' and $item["menge"] > 1){
+       if($_GET['action']=='decrease' and $item["menge"] > 1)
+       {
          $item["menge"]--;
        }
-       elseif($_GET['action']=='increase'){
+       elseif($_GET['action']=='increase')
+       {
          $item["menge"]++;
        }
        $updateItem =$pdo->prepare("UPDATE `warenkorb` SET `menge`=:menge WHERE `benutzerid` = :user AND `produktid` = :product;");
@@ -44,130 +45,127 @@ $pdo = new PDO('mysql:host=localhost;dbname=thejuicebox', 'root', '');
 
 <!DOCTYPE html>
 <html lang="de">
-
-<head>
+  <head>
     <!-- Bootstrap Abhängikeiten -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <script src="bootstrap/js/jquery.min.js"></script>
-  <script src="bootstrap/js/popper.min.js"></script>
-  <script src="bootstrap/js/bootstrap.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Default Settings -->
-  <title>The Juice Box</title>
-  <meta charset="UTF-8">
-  <link rel="icon" type="image/png" href="images/icon/smoothie64.png">
-  <link rel="stylesheet" href="css/default.css">
-  <!-- Sonstige Settings -->
-  <script src="js/logged_in.js"></script>
-  <link rel="stylesheet" href="css/warenkorb.css">
-</head>
-<body>
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-    <a class="navbar-brand" href="main.php"><img src="images/icon/smoothie512.png" width="35" height="35" alt=""></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="warenkorb.php">Warenkorb</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="bestellungen.php">Meine Bestellungen</a>
-      </li>
-    </ul>
-    
-    <a class="btn btn-outline-primary" href="logout.php" role="button">Logout</a>     
-  </span>
-  </nav>
-</header> <!-- Ende der Navigation  -->
-<main>
-  <?php
-  if (isset($_GET['action']) and $_GET['action']=='ordersuccess')
-    {
-      echo "<div class='alert alert-success' role='alert'>
-      Ihre Bestellung wurde erfolgreich aufgegeben!
-      </div>";
-    }
-  ?>
-  <div class="row">
-    <div class="leftcolumn">
-      <div class="product-table">
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Smoothies</th>
-              <th scope="col"></th>
-              <th scope="col">Einzelpreis</th>
-              <th scope="col">Anzahl</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach ($userWarenkorb as $product): ?>
-            <tr>
-              <td class="image-tablecell"><center><img src="images/products/product<?php echo $product["produktid"]?>.png" alt ="" class="produkt-img"></center></td>
-              <th scope="row"><?php echo $products[$product["produktid"]-1]["hersteller"]." ".$products[$product["produktid"]-1]["produktname"]?></th>
-              <td><?php echo $products[$product["produktid"]-1]["preis"]." €"?></td>
-              <td>
-                <label name="<?php echo $product["id"]?>"><?php echo $product["menge"]?></label>
-                <a href="?action=increase&id=<?php echo $product["produktid"] ?>" class="btn btn-outline-secondary btn-count">+</a>
-                <a href="?action=decrease&id=<?php echo $product["produktid"] ?>" class="btn btn-outline-secondary btn-count">-</a>
-              </td>
-              <td><a href="?action=delete&id=<?php echo $product["id"] ?>"><img src="images/icon/delete.png" width=30 height=auto></a></td>
-            </tr>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-    <div class="rightcolumn">
-      <div class="checkout-sum">
-      <?php 
-      $anzahlProdukte = 0;
-      $gesamtSumme = 0.0;
-      foreach ($userWarenkorb as $product){
-        $anzahlProdukte = $anzahlProdukte + $product["menge"];
-        $gesamtSumme = $gesamtSumme + $product["menge"]*$products[$product["produktid"]-1]["preis"];
-      }
-      echo "Summe (".$anzahlProdukte." Artikel): <strong>".$gesamtSumme."€</strong>";
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Default Settings -->
+    <title>The Juice Box</title>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="images/icon/smoothie64.png">
+    <link rel="stylesheet" href="css/default.css">
+    <!-- Sonstige Settings -->
+    <script src="js/logged_in.js"></script>
+    <link rel="stylesheet" href="css/warenkorb.css">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <a class="navbar-brand" href="main.php"><img src="images/icon/smoothie512.png" width="35" height="35" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="warenkorb.php">Warenkorb</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="bestellungen.php">Meine Bestellungen</a>
+            </li>
+          </ul>  
+          <a class="btn btn-outline-primary" href="logout.php" role="button">Logout</a>
+        </div>  
+      </nav>
+    </header> <!-- Ende der Navigation  -->
+    <main>
+      <?php
+        if (isset($_GET['action']) and $_GET['action']=='ordersuccess')
+        {
+          echo "<div class='alert alert-success' role='alert'>
+          Ihre Bestellung wurde erfolgreich aufgegeben!
+          </div>";
+        }
       ?>
+      <div class="row">
+        <div class="leftcolumn">
+          <div class="product-table">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Smoothies</th>
+                  <th scope="col"></th>
+                  <th scope="col">Einzelpreis</th>
+                  <th scope="col">Anzahl</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach ($userWarenkorb as $product): ?>
+                  <tr>
+                    <td class="image-tablecell"><center><img src="images/products/product<?php echo $product["produktid"]?>.png" alt ="" class="produkt-img"></center></td>
+                    <th scope="row"><?php echo $products[$product["produktid"]-1]["hersteller"]." ".$products[$product["produktid"]-1]["produktname"]?></th>
+                    <td><?php echo $products[$product["produktid"]-1]["preis"]." €"?></td>
+                    <td>
+                      <label name="<?php echo $product["id"]?>"><?php echo $product["menge"]?></label>
+                      <a href="?action=increase&id=<?php echo $product["produktid"] ?>" class="btn btn-outline-secondary btn-count">+</a>
+                      <a href="?action=decrease&id=<?php echo $product["produktid"] ?>" class="btn btn-outline-secondary btn-count">-</a>
+                    </td>
+                    <td><a href="?action=delete&id=<?php echo $product["id"] ?>"><img src="images/icon/delete.png" width=30 height=auto></a></td>
+                  </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="rightcolumn">
+          <div class="checkout-sum">
+            <?php 
+              $anzahlProdukte = 0;
+              $gesamtSumme = 0.0;
+              foreach ($userWarenkorb as $product){
+                $anzahlProdukte = $anzahlProdukte + $product["menge"];
+                $gesamtSumme = $gesamtSumme + $product["menge"]*$products[$product["produktid"]-1]["preis"];
+              }
+              echo "Summe (".$anzahlProdukte." Artikel): <strong>".$gesamtSumme."€</strong>";
+            ?>
+          </div>
+          <div class="checkout-form">
+            <form action="order.php?action=order" method="post">
+              <div class="form-group">
+                <label for="name">Vorname und Nachname</label>
+                <input type="text" class="form-control" id="name" name="name">
+              </div>
+              <div class="form-group">
+                <label for="anschrift">Anschrift</label>
+                <input type="text" class="form-control" id="anschrift" placeholder="Firma, c/o, Gebäude, Zusatzinfo" name="anschrift">
+                <input type="text" class="form-control mt-1" id="straße" placeholder="Straße und Hausnummer" name="strasse">
+              </div>
+              <div class="form-group">
+                <label for="plz">Postleitzahl</label>
+                <input type="text" class="form-control" id="plz" name="plz">
+              </div>
+              <div class="form-group">
+                <label for="stadt">Stadt</label>
+                <input type="text" class="form-control" id="stadt" name="stadt">
+              </div>
+              <div class="form-check mb-2">
+                <input type="checkbox" class="form-check-input" name="expressCheck" id="expressCheck">
+                <label class="form-check-label" for="expressCheck">Express-Versand (zzgl. 5 €)</label>
+              </div>
+              <button type="submit" class="btn btn-outline-primary">Jetzt kostenpflichtig bestellen</button>
+            </form> 
+          </div>
+        </div>
       </div>
-      <div class="checkout-form">
-        <form action="order.php?action=order" method="post">
-          <div class="form-group">
-            <label for="name">Vorname und Nachname</label>
-            <input type="text" class="form-control" id="name" name="name">
-          </div>
-          <div class="form-group">
-            <label for="anschrift">Anschrift</label>
-            <input type="text" class="form-control" id="anschrift" placeholder="Firma, c/o, Gebäude, Zusatzinfo" name="anschrift">
-            <input type="text" class="form-control mt-1" id="straße" placeholder="Straße und Hausnummer" name="strasse">
-          </div>
-          <div class="form-group">
-            <label for="plz">Postleitzahl</label>
-            <input type="text" class="form-control" id="plz" name="plz">
-          </div>
-          <div class="form-group">
-            <label for="stadt">Stadt</label>
-            <input type="text" class="form-control" id="stadt" name="stadt">
-          </div>
-          <div class="form-check mb-2">
-            <input type="checkbox" class="form-check-input" name="expressCheck" id="expressCheck">
-            <label class="form-check-label" for="expressCheck">Express-Versand (zzgl. 5 €)</label>
-          </div>
-          <button type="submit" class="btn btn-outline-primary">Jetzt kostenpflichtig bestellen</button>
-        </form> 
+    </main>
+    <footer>
+      <div class="logged-in-text">
+        <span id="logged_in"></span>&nbsp;<br>
       </div>
-    </div>
-  </div>
-</main>
-<footer>
-  <div class="logged-in-text">
-    <span id="logged_in"></span>&nbsp;<br>
-  </div>
-</footer>
-</body>
+    </footer>
+  </body>
 </html>
