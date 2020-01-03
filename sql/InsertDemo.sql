357dfece3eace8a1412e1c57507937c78242de18
MarkPhilipps <m.h.philipps@gmail.com>
2020-01-03 10:32:56 +0100

    SQL Demo erweitert.

--- sql/InsertDemo.sql
+++ sql/InsertDemo.sql
@@ -15,3 +15,18 @@ INSERT INTO `produkt` (`id`,`produktname`, `preis`, `beschreibung`, `hersteller`
 INSERT INTO `benutzer` (`id`,`email`,`passwort`,`salz`,`vorname`,`nachname`) VALUES
 (1,'test@test.de','1b3df60c735116b37986b67b1a006924785d009c9476f4206e773b1df7eb308cb94700e4bba2d0c7b9443bde6b95a902055cb2378b276c15d4a0bdb39d61432e','19594914065df61406b6ea05.32069662', 'TestVorname', 'TestNachname'),
 (2, 'thejuiceboxwi3@gmail.com', 'de5c6f004cc93c8392c2455f742917115086adae14199e62acce2035f82f21854332ddb5be78b19e96eec23af73d075f55c04678d73ae6ef42a22fedaf88c64f', '17642453675dfe5a62d72506.96682176', 'Keine', 'Ahnung');
+
+INSERT INTO `warenkorb` (`id`,`benutzerid`,`produktid`,`menge`) VALUES
+(1,1,6,4),
+(2,1,1,2),
+(3,1,11,10);
+
+INSERT INTO `bestellung` (`id`,`benutzerid`,`expresslieferung`,`bestelldatum`,`summe`,`vornachname`,`zusatzinfo`,`strasse`,`plz`,`ort`) VALUES
+(1,1,1,"2019-12-27 14:49:48",20.73,"Hochschule Reutlingen","Fakultät Informatik","Alteburgstraße 150","72762","Reutlingen");
+
+INSERT INTO `bestellung_hat_produkte` (`bestellungid`,`produktid`,`menge`) VALUES
+(1,2,1),
+(1,4,3),
+(1,5,1),
+(1,10,1),
+(1,12,1);
